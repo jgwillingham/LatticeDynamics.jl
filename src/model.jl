3c8f4ecbdb55e4fc4781c72ad8e8d2fde3c5f93d
jgwillingham <jwillin@tulane.edu>
2021-02-04 22:23:24 -0500

    Improved plotting with keyword args. Changed default units to meV instead of THz

--- src/model.jl
+++ src/model.jl
@@ -51,11 +51,12 @@ function getDispersion(qPath::Array, crystal::Union{Crystal, Slab}, couplings::A
         𝔻List = map(x -> Hermitian(Complex.(𝕄*x*𝕄)), 𝕕List)
         ω²Values = map(x -> round.(x, digits=10), map(eigvals, 𝔻List))
         fValues = map( x -> .√x./(2π), ω²Values)
-        return fValues
+        meVDispersion = 4.13567 .*fValues # convert THz to meV
+        return meVDispersion
 end
 
 
-function plotDispersion(dispersion::Array, qPathParts::Array=[], qLabels::Array=[])
+function plotDispersion(dispersion::Array, qPathParts::Array=[], qLabels::Array=[]; ylims::Array=[0.0, Inf], color::Symbol=:steelblue, size::Tuple=(600,350), title::String="")
     bands = []
 
     for i in 1:length(dispersion[1])
@@ -63,10 +64,10 @@ function plotDispersion(dispersion::Array, qPathParts::Array=[], qLabels::Array=
         t = map(real, t)
         append!(bands, [t])
     end
-    plot(bands, linewidth=2, xticks=(qPathParts, qLabels), legend=false, xtickfont=(13), size=(800, 400))
+    plot(bands, linewidth=2, xticks=(qPathParts, qLabels), legend=false, xtickfont=(13), size=size, color=color, )
     plot!(qPathParts, seriestype=:vline, color=:black, linealpha=0.35)
     xlims!((1.0, qPathParts[end]))
-    ylims!(0, Inf)
-    title!("Phonon Dispersion")
-    ylabel!("Frequency (THz)")
+    ylims!(ylims...)
+    title!(title)
+    ylabel!("Energy (meV)")
 end
